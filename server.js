260168b058c0a24e930505b860e90a5ca7015034
SOulReaver1 <ilan.journo555@gmail.com>
2020-12-02 00:35:59 +0100

    init

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+
+var global = [];
+
+app.get('/tasks', (req, res) => {
+  res.json(global)
+})
+
+app.post('/tasks', (req, res) => {
+    const name = req.body.name;
+    const date = req.body.date;
+    const finish = req.body.finish;
+
+    if(name && date && finish){
+        global.push(req.body);
+        res.json(global);
+    }else{
+        res.send('Une erreur est survenue');
+    }
+
+})
+
+app.get('/tasks/:uid', (req, res) => {
+    global[req.params.uid - 1] ? res.json(global[req.params.uid - 1]) : res.send('Une erreur est survenue');
+})
+
+app.put('/tasks/:uid', (req, res) => {
+    if(global[req.params.uid - 1] && req.body.name && req.body.date && req.body.finish){
+        global[req.params.uid - 1] = req.body;
+        res.json(global[req.params.uid - 1]);
+    }else{
+        res.send('Une erreur est survenue');
+    }
+})
+
+app.delete('/tasks/:uid', (req, res) => {
+    if(global[req.params.uid - 1]){
+        global.splice(req.params.uid - 1, 1);
+        res.json(global);
+    }else{
+        res.send('Une erreur est survenue');
+    }
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
